a67ecbe266f98c87f2af2792b502ead0b291c965
Nicolas Girard <nicolasg37@gmail.com>
2016-07-03 19:52:36 +0200

    update de la route put sur /users. suppression de post

--- routes/users/users.js
+++ routes/users/users.js
@@ -257,7 +257,6 @@ router.delete("/:idUser", expressjwt({secret: process.env.jwtSecretKey}), functi
   *           "idUser": 1,
   *           "email": "j.dujardin@gmail.com",
   *           "pseudo": "JeanJean",
-  *           "password": "a94a8fe5ccb19ba61c4c0873d391e9879ffa353a",
   *           "lastName": "Dujardin",
   *           "firstName": "Jean",
   *           "role": 4,
@@ -288,11 +287,11 @@ router.put("/:idUser", expressjwt({secret: process.env.jwtSecretKey}), function(
                     res.json({"Error" : true, "Code" : 103}); // L'utilisateur n'existe pas
                 else
                 {
-                    var query = "UPDATE Users SET `email`= ?,`pseudo`= ?, `password`= ?,`lastName`= ?,`firstName`= ?,`role`= ? WHERE `idUser` = ?";
-                    var table = [req.body.email, req.body.pseudo, sha1(req.body.password), req.body.lastName, req.body.firstName, req.body.role, req.params.idUser];
+                    var query = "UPDATE Users SET `email`= ?,`pseudo`= ?,`lastName`= ?,`firstName`= ?,`role`= (SELECT idRole FROM Roles WHERE role = ?), `profilePicture` = (SELECT idMedia FROM Medias WHERE url = ?) WHERE `idUser` = ?";
+                    var table = [req.body.email, req.body.pseudo, req.body.lastName, req.body.firstName, req.body.role, req.body.profilePicture, req.params.idUser];
 
                     query = mysql.format(query, table);
-                    connection.query(query, function(err, rows){
+                    req.app.locals.connection.query(query, function(err, rows){
                         if (!err)
                         {
                             res.json({"Error" : false, "Code" : 1, "Users" : rows[0]}); // OK
@@ -308,58 +307,6 @@ router.put("/:idUser", expressjwt({secret: process.env.jwtSecretKey}), function(
     } else {
         res.json({"Error": true, "Code" : 105}); // L'utilisateur n'a pas les droits
     }
-});
-
-/**
- * @api {post} /users/ Ajouter un utilisateur
- * @apiVersion 1.0.0
- * @apiName Ajout d'un utilisateur
- * @apiGroup Gestion Utilisateurs
- * @apiDescription Ajoute un utilisateur après avoir vérifier qu'il n'existait pas.
- *
- * @apiParam {String} email Adresse mail de l'utilisateur
- * @apiParam {string} pseudo Le pseudonyme de l'utilisateur
- * @apiParam {String} password Mot de passe de l'utilisateur
- * @apiParam {String} lastName Nom de l'utilisateur
- * @apiParam {String} firstName Prénom de l'utilisateur
- * @apiParam {String} role Rôle de l'utilisateur
- *
- * @apiSuccess (Succès) {Boolean} Error Retourne "false" en cas de réussite
- * @apiSuccess (Succès) {Number} Code Code d'erreur (1 = Aucune erreur détectée)
- *
- * @apiSuccessExample Succès - Réponse :
- *     {
-  *       "Error": false,
-  *       "Code" : 1,
-  *     }
- *
- * @apiError (Erreur) {Boolean} Error Retourne "true" en cas d'erreur
- * @apiError (Erreur) {Number} Code Code d'erreur (101 = L'utilisateur existe déjà, 102 = Erreur lors de la requête)
- *
- * @apiErrorExample Erreur - Réponse :
- *     {
-  *       "Error" : true,
-  *       "Code" : 101
-  *     }
- *
- */
-router.post("/", function(req,res){
-    var query = "INSERT INTO ??(??,??,??,??,??,??) VALUES (?,?,?,?,?,?)";
-    var table = ["Users", "email", "pseudo", "password", "lastName", "firstName", "role",
-        req.body.email, req.body.pseudo, sha1(req.body.password), req.body.lastName, req.body.firstName, req.body.role];
-
-    query = mysql.format(query,table);
-
-    req.app.locals.connection.query(query,function(err,rows){
-        if (!err)
-            res.json({"Error" : false, "Code" : 1}); // OK
-        else {
-            if (err.code == "ER_DUP_ENTRY")
-                res.json({"Error" : true, "Code" : 101}); // L'utilisateur existe déjà
-            else
-                res.json({"Error" : true, "Code" : 102}); // Erreur
-        }
-    });
 });
 
 module.exports = router;
